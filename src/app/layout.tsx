ee52cadfe5478e81c480686c37f339e1866ed13a
K G A N <86733485+kganallinone@users.noreply.github.com>
2026-08-29 21:24:15 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono, Manrope } from "next/font/google";
-import "./globals.css";
 
 const manrope = Manrope({
   variable: "--font-sans",
@@ -14,11 +13,11 @@ const jetbrainsMono = JetBrains_Mono({
 
 export const metadata: Metadata = {
   title: {
-    default: "FTCC Solutions Inc.",
-    template: "%s | FTCC Solutions Inc.",
+    default: "Schedule Autofill for Google Forms",
+    template: "%s | Schedule Autofill for Google Forms",
   },
   description:
-    "FTCC Solutions Inc. is a leading healthcare technology company and a proud partner of PhilHealth under the Yakap Program. We are dedicated to transforming the healthcare landscape through innovative solutions that improve patient care, streamline operations, and enhance healthcare outcomes for providers and communities.",
+    "Autofill your Google Forms schedules with ease. Save time and reduce errors with our automated solution.",
   icons: {
     icon: "/logo.png",
     shortcut: "/logo.png",
